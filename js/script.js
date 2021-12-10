8fdb2e4e58452df6195595df7935eeefefcf0d8e
Andrea <miltonruiz019@gmail.com>
2021-12-10 11:04:06 -0600

    10Dic2021

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*--------------------------
+      Calling Carousel
+---------------------------*/
+var myCarousel = document.querySelector('#carouselExampleFade')
+var carousel = new bootstrap.Carousel(carouselExampleFade);
+
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+        $('.navbar').css('background', 'rgba(0,0,0,0.5)');
+    } else{
+        $('.navbar').css('background', '#000000');
+    }
+});
+
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if(scroll < 300){
+        $('.nav-link').css('color', 'black');
+    } else{
+        $('.nav-link').css('color', '#d4dae2');
+    }
+});
