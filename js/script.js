05385bbf14fe63837d760da96d2d87f08b734e4e
Andrea <miltonruiz019@gmail.com>
2021-12-30 19:10:52 -0600

    29 dic 2021

--- js/script.js
+++ js/script.js
@@ -9,12 +9,66 @@ $(document).ready(function() {
    });
 });
 
+/*------------------
+   Animations
+--------------------*/
+function fadeup() {
+  var fadeups = document.querySelectorAll(".fadeup");
+
+  for (var i = 0; i < fadeups.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = fadeups[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      fadeups[i].classList.add("active");
+    } else {
+      fadeups[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", fadeup);
+
+/*-------*/
+function zoomin() {
+  var zoomins = document.querySelectorAll(".zoomin");
+
+  for (var i = 0; i < zoomins.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = zoomins[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      zoomins[i].classList.add("active");
+    } else {
+      zoomins[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", zoomin);
+/*-------*/
+function rotate() {
+  var rotates = document.querySelectorAll(".rotate");
+
+  for (var i = 0; i < rotates.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = rotates[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      rotates[i].classList.add("active");
+    } else {
+      rotates[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", rotate);
 /*--------------------------
       Calling AOS Animate
 ---------------------------*/
-AOS.init({
-  easing: 'ease-in-out-sine'
-});
+// AOS.init({
+//   easing: 'ease-in-out-sine'
+// });
 
 /*--------------------------
       Calling Carousel
